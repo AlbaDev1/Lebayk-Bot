a3e37e9f2e4e97f0b01a11f8d4b7a75011d620c2
AlbaDev1 <albaishere.contact@gmail.com>
2022-08-14 11:20:56 +0200

    New Version
    - Added Twitch Notifications
    - Added Youtube Notifications

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new Client({
 
 client.on('ready', async (client) => {
 
-  const guild = client.guilds.cache.get('992833515882283069')
+  const guild = client.guilds.cache.get('980021196924006430')
 
   /*const userIds = new Set();
 
@@ -41,6 +41,81 @@ client.on('ready', async (client) => {
   }
 
   const membrestotal = userIds.size;*/
+
+  // Notifications Youtube
+  const { getChannelVideos } = require('yt-channel-info')
+  const db = require("megadb")
+  const yt = new db.crearDB("yt")
+  setInterval(async function(){
+    const videos = await getChannelVideos("UC8avyyaUes-7YU_uVxXDjzg", 0)
+    const thevideo = videos.items[0]
+    const title = await yt.obtener("UC8avyyaUes-7YU_uVxXDjzg")
+    if(title === thevideo.title) return
+    if(title !== thevideo.title){
+      yt.establecer("UC8avyyaUes-7YU_uVxXDjzg", thevideo.title)
+      client.channels.cache.get("980021197871910921").send(`${thevideo.author} a sorti une nouvelle vidéo : **${thevideo.title}**\nhttps://www.youtube.com/watch?v=${thevideo.videoId}`)
+    }
+  }, 120000)
+
+  // Notifications Twitch
+  const TwitchApi = require("node-twitch").default;
+
+  const twitch = new TwitchApi({
+	  client_id: process.env.TWITCH_ID,
+	  client_secret: process.env.TWITCH_TOKEN
+  });
+
+  setInterval(async function(){
+    await twitch.getStreams({ channel: "lebayk" }).then(async data => {
+        const r = data.data[0]
+
+        const avatar = "https://static-cdn.jtvnw.net/jtv_user_pictures/81852c39-8dd3-431b-b0b0-2ded25da8e11-profile_image-70x70.jpeg"
+
+        const schema = require("./schemas/twitchSchema")
+
+        if(r !== undefined) {
+            if(r.type === "live"){
+              const embed = new Discord.MessageEmbed()
+              .setAuthor(`${r.user_name}`)
+              .setTitle(`${r.title}`)
+              .setURL(`https://twitch.tv/${r.user_name}`)
+              .addField("Game", `${r.game_name}`)
+              .addField("Viewers", `${r.viewer_count}`)
+              .setImage(`${r.getThumbnailUrl()}`)
+              .setColor("BLURPLE")
+
+              let data2 = await schema.findOne({ user: r.user_name, title : r.title})
+              
+              if(!data2){
+                const newdata = new schema({
+                  user: r.user_name,
+                  title: `${r.title}`
+                })
+
+                await client.channels.cache.get("980021197871910921").send({ content: `${r.user_name} est en live ! Venez nous rejoindre ! \nhttps://twitch.tv/${r.user_name}`, embeds: [embed]})
+
+                return await newdata.save()
+              }
+
+              if(data2.title === `${r.title}`) return
+
+              await client.channels.cache.get("980021197871910921").send({ content: `${r.user_name} est en live ! Venez nous rejoindre ! \nhttps://twitch.tv/${r.user_name}`, embeds: [embed]})
+              await schema.findOneAndUpdate({ user: r.user_name }, { title: r.title })
+            }
+        }
+    })
+}, 120000)
+
+  mongoose.connect("mongodb+srv://albadev:lucas2012@cluster0.ed4gwtl.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+  }).then(() => {
+    console.log("Connecté a MongoDB !")
+  }).catch((err) => {
+    console.log("Une erreur est survenue lors de la connexion a MongoDB.")
+    console.log("------------------------ erreur : ------------------------")
+    console.log(err)
+    console.log("----------------------------------------------------------")
+  })
 
   const status = [
     "Minecraft",
